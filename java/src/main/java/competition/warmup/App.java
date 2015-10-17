2f35d6157b48349b907e669ce748557c3af405dd
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2015-10-17 13:31:44 +0100

    Update the Java warmup to spec 0.2

--- java/src/main/java/competition/warmup/App.java
+++ java/src/main/java/competition/warmup/App.java
@@ -1,6 +1,8 @@
 package competition.warmup;
 
-import competition.client.Client;
+
+import tdl.client.Client;
+import tdl.client.abstractions.ImplementationMap;
 
 /**
  * Created by julianghionoiu on 23/08/2015.
@@ -9,11 +11,14 @@ public class App {
 
 
     public static void main(String[] args) throws Exception {
-        Client client = new Client("tcp://localhost:61616", "jgh");
+        Client client = new Client("localhost", 61616, "jgh");
 
-        client.trialRunWith(params ->
+        ImplementationMap implementations = new ImplementationMap();
+        implementations.register("sum", params ->
                 sum(Integer.valueOf(params[0]), Integer.valueOf(params[1]))
         );
+
+        client.goLiveWith(implementations);
     }
 
     public static Integer sum(Integer x, Integer y) {
