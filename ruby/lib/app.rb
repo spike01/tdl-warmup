f48fd21a538da0f193e90d15ddb2c4bc88f86ee7
CrowdHailer <peterhsaxton@gmail.com>
2015-12-10 20:56:57 +0000

    Ruby warmup uses version 0.3.6

--- ruby/lib/app.rb
+++ ruby/lib/app.rb
@@ -2,35 +2,45 @@ require 'tdl'
 require 'ostruct'
 require 'json'
 
+
 class App
+
+  #  STEP 1: Set username as your name
+  def username
+    "username"
+  end
+
+  # STEP 2: Implement a function that takes a number and returns the next number.
+  # Uncomment this implementation or write your own.
+  def increment(i)
+    i + 1
+  end
+
+  # STEP 3: Once happy with your implementation go live with you solution.
+  def ready?
+    false
+  end
+
+  def display_description(description)
+    puts description
+    "OK"
+  end
+
+  def display_required_methods(required_methods)
+    required_methods
+    "OK"
+  end
+
 
   def main()
     # Starts a new TDL client TDL::Client.new(host, port, username);
-    client = TDL::Client.new('172.20.10.2', 61613, 'username')
-
-    solution = OpenStruct.new({
-      display_description: ->(description){ puts description; "OK" },
-      display_required_methods: ->(required_methods){ puts required_methods; "OK" },
-
-      # STEP 1: Implement a function that takes a number and returns the next number.
-
-      # Uncomment this implementation or write your own.
-      # increment: ->(x){ x+1 }
-    })
-    def solution.method_missing(meth, *other)
-      ->(*args){ puts "method missing #{meth}"}
+    client = TDL::Client.new('172.20.10.2', 61613, username)
+    puts "hello"
+    if ready?
+      client.go_live_with self
+    else
+      client.trial_run_with self
     end
-    def solution.respond_to(*a)
-      true
-    end
-
-    # STEP 2: Once happy with your implementation go live with you solution.
-    # Uncomment this line and comment out the trial run.
-    # client.go_live_with solution
-    client.trial_run_with solution
   end
 
-  def self.sum(x, y)
-    nil
-  end
 end
