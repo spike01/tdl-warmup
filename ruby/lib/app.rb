cdaea40cf4c7d68cd31e11bb5e5e82b2b100c0ae
CrowdHailer <peterhsaxton@gmail.com>
2016-01-21 20:27:59 +0000

    update ruby example for 0.6 client implementation

--- ruby/lib/app.rb
+++ ruby/lib/app.rb
@@ -1,46 +1,12 @@
-require 'tdl'
-require 'ostruct'
-require 'json'
-
-
 class App
-
-  #  STEP 1: Set username as your name
-  def username
-    "username"
+  def display(content)
+    puts content
+    'OK'
   end
 
   # STEP 2: Implement a function that takes a number and returns the next number.
   # Uncomment this implementation or write your own.
-  # def increment(i)
-  #   i + 1
-  # end
-
-  # STEP 3: Once happy with your implementation go live with you solution.
-  def ready?
-    false
+  def increment(i)
+    i + 1
   end
-
-  def display_description(description)
-    puts description
-    "OK"
-  end
-
-  def display_required_methods(required_methods)
-    required_methods
-    "OK"
-  end
-
-
-  def main()
-    # Starts a new TDL client TDL::Client.new(host, port, username);
-    client = TDL::Client.new('172.20.10.2', 61613, username)
-
-    if ready?
-      client.go_live_with self
-    else
-      client.trial_run_with self
-    end
-  end
-
 end
--- ruby/lib/run.rb
+++ ruby/lib/run.rb
@@ -1,12 +1,22 @@
+require 'tdl'
 require 'logging'
 Logging.logger.root.appenders = Logging.appenders.stdout
 Logging.logger.root.level = :info
 
 require_relative './app'
+app = App.new
 
-require 'logging'
-Logging.logger.root.appenders = Logging.appenders.stdout
-Logging.logger.root.level = :info
+#  STEP 1: Set the username as your name
+client = TDL::Client.new('172.20.10.2', 61613, 'peter')
 
-app = App.new()
-app.main()
+include TDL::ClientActions
+rules = TDL::ProcessingRules.new
+rules.on('display_description').call(app.method(:display)).then(publish)
+rules.on('display_required_methods').call(app.method(:display)).then(publish)
+
+# STEP 3: Once happy with your implementation go live with you solution.
+
+# rules.on('increment').call(app.method(:increment)).then(publish)
+rules.on('increment').call(app.method(:increment)).then(stop)
+
+client.go_live_with(rules)
