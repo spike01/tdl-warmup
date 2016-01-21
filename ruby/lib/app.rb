5eac64a45663fa462a32ad8c004939ece2de9f4e
CrowdHailer <peterhsaxton@gmail.com>
2016-01-21 20:56:10 +0000

    recomment demo code

--- ruby/lib/app.rb
+++ ruby/lib/app.rb
@@ -6,7 +6,7 @@ class App
 
   # STEP 2: Implement a function that takes a number and returns the next number.
   # Uncomment this implementation or write your own.
-  def increment(i)
-    i + 1
-  end
+  # def increment(i)
+  #   i + 1
+  # end
 end
