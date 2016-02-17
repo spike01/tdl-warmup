4ab999fc5a8f2be36c40a92faa8e886df770f9cf
CrowdHailer <peterhsaxton@gmail.com>
2016-02-17 20:59:58 +0000

    ruby warmup handles 0.8 client

--- ruby/lib/app.rb
+++ ruby/lib/app.rb
@@ -1,12 +1,7 @@
 class App
-  def display(content)
-    puts content
-    'OK'
-  end
-
   # STEP 2: Implement a function that takes a number and returns the next number.
   # Uncomment this implementation or write your own.
-  # def increment(i)
-  #   i + 1
-  # end
+  def increment(i)
+    i + 1
+  end
 end
--- ruby/lib/run.rb
+++ ruby/lib/run.rb
@@ -7,17 +7,23 @@ require_relative './app'
 app = App.new
 
 #  STEP 1: Set the username as your name
-client = TDL::Client.new(hostname: '172.20.10.2', username: 'username')
+client = TDL::Client.new(hostname: '172.20.10.2', username: 'peter')
 
 include TDL::ClientActions
 rules = TDL::ProcessingRules.new
 
-rules.on('display_description').call(app.method(:display)).then(publish)
-rules.on('display_required_methods').call(app.method(:display)).then(publish)
+rules
+  .on('display_description')
+  .call(->(label, details){
+    puts label
+    puts details
+    'OK'
+  })
+  .then(publish)
 
 # STEP 3: Once happy with your implementation go live with you solution.
 
-# rules.on('increment').call(app.method(:increment)).then(publish)
-rules.on('increment').call(app.method(:increment)).then(stop)
+rules.on('increment').call(app.method(:increment)).then(publish)
+# rules.on('increment').call(app.method(:increment)).then(stop)
 
 client.go_live_with(rules)
